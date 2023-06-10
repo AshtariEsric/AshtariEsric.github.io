438b4d105e12d9150bbd811e6621cf4fb71e631d
Apix Denice mini <dennishasselbusch@icloud.com>
2023-06-10 12:56:39 +0200

    d2

--- assets/js/codescript.js
+++ assets/js/codescript.js
@@ -1,11 +1,11 @@
-// Swift-Code
+// Swift-Code mit Umbrüchen
 const swiftCode = `import UIKit
 
 func sayHello() {
     print("Hello, World!")
 }
 
-sayHello()`;
+sayHello();`;
 
 // Animation des Swift-Codes
 const codeContainer = document.getElementById('code-container');
@@ -14,7 +14,11 @@ let currentCodeIndex = 0;
 function showNextCharacter() {
     if (currentCodeIndex < swiftCode.length) {
         const nextCharacter = swiftCode.charAt(currentCodeIndex);
-        codeContainer.innerHTML += nextCharacter;
+        if (nextCharacter === '\n') {
+            codeContainer.innerHTML += '<br>';
+        } else {
+            codeContainer.innerHTML += nextCharacter;
+        }
         currentCodeIndex++;
         setTimeout(showNextCharacter, 50); // Hier kannst du die Geschwindigkeit der Animation anpassen
     }
